df229669e8d35d57a426816218de42e7d7e5cd02
Hemant_Bhambhu <hkbhambhu@gmail.com>
2023-06-18 11:58:28 +0530

    added training.py file to dictate how the models will be trained

---
+++ Assignment_7/training.py
@@ -0,0 +1,46 @@
+# importing modules
+import torch
+import torch.nn.functional as F
+
+
+def train(model,device,train_loader,optimizer,epoch):
+    model.train()
+    correct = 0
+    processed = 0
+    for batch_idx, (data, target) in enumerate(train_loader):
+        # move the data to device
+        data, target = data.to(device), target.to(device)
+        # zero the gradients
+        optimizer.zero_grad()
+        # forward pass
+        output = model(data)
+        # calculate the loss
+        loss = F.nll_loss(output, target)
+        # backward pass
+        loss.backward()
+        # update the weights
+        optimizer.step()
+        # print the progress
+        correct += output.argmax(dim=1).eq(target).sum().item()
+        processed += len(data)
+        if batch_idx % 100 == 0:
+            print(f'Epoch: {epoch+1} loss is {loss.item()} Accuracy: {100*correct/processed:0.2f}')
+
+def test(model,device,test_loader):
+    model.eval()
+    correct = 0
+    test_loss = 0
+    with torch.no_grad():
+        for data,target in test_loader:
+            data,target = data.to(device),target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output,target,reduction='sum').item()
+            pred = output.argmax(dim=1,keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    test_loss /= len(test_loader.dataset)
+    print(f'Test loss is {test_loss} and accuracy is {100*correct/len(test_loader.dataset)}')
+
+def run(model,device,train_loader,test_loader,optimizer,epochs):
+    for epoch in range(epochs):
+        train(model,device,train_loader,optimizer,epoch)
+        test(model,device,test_loader)
