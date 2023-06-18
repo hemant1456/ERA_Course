0799ef93df8aae28c4a38fc6faba7657975b21f0
Hemant_Bhambhu <hkbhambhu@gmail.com>
2023-06-18 16:34:04 +0530

    added scheduler in training.py, modified model to reach receptive field of 28

--- Assignment_7/Models.py
+++ Assignment_7/Models.py
@@ -5,29 +5,29 @@ import torch.nn.functional as F
 class Model1(nn.Module):
     def __init__(self):
         super(Model1,self).__init__()
-        self.convblock1 = nn.Sequential(nn.Conv2d(in_channels=1,out_channels=8,kernel_size=(3,3),padding=1,bias=False),
+        self.convblock1 = nn.Sequential(nn.Conv2d(in_channels=1,out_channels=4,kernel_size=(3,3),padding=1,bias=False),
+                                        nn.ReLU(),
+                                        nn.BatchNorm2d(4),
+                                        nn.Dropout(0.1)) #28 rf=3
+        self.convblock2 = nn.Sequential(nn.Conv2d(in_channels=4,out_channels=8,kernel_size=(3,3),bias=False),
                                         nn.ReLU(),
                                         nn.BatchNorm2d(8),
-                                        nn.Dropout(0.1)) #28
-        self.convblock2 = nn.Sequential(nn.Conv2d(in_channels=8,out_channels=16,kernel_size=(3,3),bias=False),
-                                        nn.ReLU(),
-                                        nn.BatchNorm2d(16),
-                                        nn.Dropout(0.1)) #26
+                                        nn.Dropout(0.1)) #26 rf=5
         self.pool1= nn.MaxPool2d(2,2) #13
-        self.convblock3 = nn.Sequential(nn.Conv2d(in_channels=16,out_channels=8,kernel_size=(3,3),bias=False),
-                                        nn.ReLU(),
-                                        nn.BatchNorm2d(8),
-                                        nn.Dropout(0.1)) #11
-        self.convblock4 = nn.Sequential(nn.Conv2d(in_channels=8,out_channels=16,kernel_size=(3,3),padding=1,bias=False),
+        self.convblock3 = nn.Sequential(nn.Conv2d(in_channels=8,out_channels=16,kernel_size=(3,3),bias=False),
                                         nn.ReLU(),
                                         nn.BatchNorm2d(16),
-                                        nn.Dropout(0.1)) #11
-        self.pool2= nn.MaxPool2d(2,2) #5
-        self.convblock5 = nn.Sequential(nn.Conv2d(in_channels=16,out_channels=8,kernel_size=(3,3),bias=False),
+                                        nn.Dropout(0.1)) #11 rf=6
+        self.convblock4 = nn.Sequential(nn.Conv2d(in_channels=16,out_channels=16,kernel_size=(3,3),padding=1,bias=False),
                                         nn.ReLU(),
-                                        nn.BatchNorm2d(8),
-                                        nn.Dropout(0.1)) #3
-        self.convblock6 = nn.Sequential(nn.Conv2d(in_channels=8,out_channels=10,kernel_size=(3,3),bias=False)) #1
+                                        nn.BatchNorm2d(16),
+                                        nn.Dropout(0.1)) #11 rf=10
+        self.pool2= nn.MaxPool2d(2,2) #5 , rf=12
+        self.convblock5 = nn.Sequential(nn.Conv2d(in_channels=16,out_channels=32,kernel_size=(3,3),bias=False),
+                                        nn.ReLU(),
+                                        nn.BatchNorm2d(32),
+                                        nn.Dropout(0.1)) #3 rf=20
+        self.convblock6 = nn.Sequential(nn.Conv2d(in_channels=32,out_channels=10,kernel_size=(3,3),bias=False)) #1 rf=28
 
     def forward(self,x):
         x = self.convblock1(x)
--- Assignment_7/training.py
+++ Assignment_7/training.py
@@ -23,8 +23,8 @@ def train(model,device,train_loader,optimizer,epoch):
         # print the progress
         correct += output.argmax(dim=1).eq(target).sum().item()
         processed += len(data)
-        if batch_idx % 100 == 0:
-            print(f'Epoch: {epoch+1} loss is {loss.item()} Accuracy: {100*correct/processed:0.2f}')
+        if batch_idx % 200 == 0:
+            print(f'Epoch: {epoch+1} and batch idx: {batch_idx} Train loss is {loss.item()} Train Accuracy: {100*correct/processed:0.2f}')
 
 def test(model,device,test_loader):
     model.eval()
@@ -38,9 +38,11 @@ def test(model,device,test_loader):
             pred = output.argmax(dim=1,keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
     test_loss /= len(test_loader.dataset)
-    print(f'Test loss is {test_loss} and accuracy is {100*correct/len(test_loader.dataset)}')
+    print(f'Test loss is {test_loss} and Test accuracy is {100*correct/len(test_loader.dataset)}')
 
-def run(model,device,train_loader,test_loader,optimizer,epochs):
+def run(model,device,train_loader,test_loader,optimizer,epochs,scheduler):
     for epoch in range(epochs):
+        print(f'\ncurrently we are at epoch {epoch+1} and learning rate used is {scheduler.get_last_lr()[0]}')
         train(model,device,train_loader,optimizer,epoch)
         test(model,device,test_loader)
+        scheduler.step()
