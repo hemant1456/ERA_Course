73189dc22507d8404f76f443af4a28d6d63bec5f
Hemant_Bhambhu <hkbhambhu@gmail.com>
2023-06-25 15:10:59 +0530

    added 3 models batch_norm group_norm and layer_norm

--- Assignment_8/model.py
+++ Assignment_8/model.py
@@ -1,56 +1,237 @@
-import torch
+from torch.nn.modules.batchnorm import BatchNorm2d
 import torch.nn as nn
 import torch.nn.functional as F
 
 class Model_batch_norm(nn.Module):
-    def __init__(self):
-        super(Model_batch_norm, self).__init__()
-        self.convblock1= nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=8, kernel_size=(3,3), bias=False),
-            nn.ReLU(),
-            nn.BatchNorm2d(8),
-            nn.Dropout(0.1)
-        ) #30
-        self.convblock2= nn.Sequential(
-            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3,3),  bias=False),
-            nn.ReLU(),
-            nn.BatchNorm2d(16),
-            nn.Dropout(0.1)
-        ) #28
-        self.pool1= nn.MaxPool2d(2,2)
-        self.convblock3= nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3,3),bias=False),
-            nn.ReLU(),
-            nn.BatchNorm2d(32),
-            nn.Dropout(0.1)
-        ) #14
-        self.convblock4= nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3,3), bias=False),
-            nn.ReLU(),
-            nn.BatchNorm2d(64),
-            nn.Dropout(0.1)
-        ) #10
-        self.pool2= nn.MaxPool2d(2,2)
-        self.convblock5= nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3), bias=False),
-            nn.ReLU(),
-            nn.BatchNorm2d(128),
-            nn.Dropout(0.1)
-        )
-        self.convblock6= nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=10, kernel_size=(3,3), bias=False),
-            nn.ReLU(),
-            nn.BatchNorm2d(10),
-            nn.Dropout(0.1)
-        )
-    def forward(self,x):
-        x= self.convblock1(x)
-        x= self.convblock2(x)
-        x= self.pool1(x)
-        x= self.convblock3(x)
-        x= self.convblock4(x)
-        x= self.pool2(x)
-        x= self.convblock5(x)
-        x= self.convblock6(x)
-        x= x.view(-1, 10)
-        return F.log_softmax(x, dim=-1)
+  def __init__(self):
+    super(Model_batch_norm,self).__init__()
+    self.convblock1 = nn.Sequential(
+        nn.Conv2d(3,16,3,bias=False,padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(16)
+        ) #32
+    self.convblock2 = nn.Sequential(
+        nn.Conv2d(16,32,3,bias=False,padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(32)
+        ) #32
+    self.convblock3 = nn.Sequential(
+        nn.Conv2d(32,16,1,bias=False,padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(16)
+        ) #32
+    self.pool1 = nn.MaxPool2d(2,2) #16
+
+    self.convblock4 = nn.Sequential(
+        nn.Conv2d(16,32,3,bias=False,padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(32)
+        ) #16
+    self.convblock5 = nn.Sequential(
+        nn.Conv2d(32,32,3,bias=False,padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(32)
+        ) #16
+    self.convblock6 = nn.Sequential(
+        nn.Conv2d(32,32,3,bias=False,padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(32)
+        ) #16
+    self.convblock7 = nn.Sequential(
+        nn.Conv2d(32,16,1,bias=False,padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(16)
+        ) #16
+
+    self.pool2= nn.MaxPool2d(2,2) #8
+
+    self.convblock8 = nn.Sequential(
+        nn.Conv2d(16,32,3,bias=False),
+        nn.ReLU(),
+        nn.BatchNorm2d(32)
+        ) #6
+    self.convblock9 = nn.Sequential(
+        nn.Conv2d(32,32,3,bias=False),
+        nn.ReLU(),
+        nn.BatchNorm2d(32)
+        ) #4
+
+    self.convblock10 = nn.Sequential(
+        nn.Conv2d(32,64,3,bias=False),
+        nn.ReLU(),
+        nn.BatchNorm2d(64)
+        ) #2
+
+    self.gap = nn.Sequential(
+        nn.AdaptiveAvgPool2d(1)
+        ) #1
+
+    self.convblock11 = nn.Sequential(
+        nn.Conv2d(64,10,1,bias=False),
+        ) #1
+  def forward(self,x):
+    x= self.pool1(self.convblock3(self.convblock2(self.convblock1(x))))
+    x= self.pool2(self.convblock7(self.convblock6(self.convblock5(self.convblock4(x)))))
+    x= self.convblock10(self.convblock9(self.convblock8(x)))
+    x= self.gap(x)
+    x=self.convblock11(x)
+    x=x.view(-1,10)
+    return F.log_softmax(x)
+  
+
+from torch.nn.modules.normalization import GroupNorm
+class Model_group_norm(nn.Module):
+  def __init__(self):
+    super(Model_group_norm,self).__init__()
+    self.convblock1 = nn.Sequential(
+        nn.Conv2d(3,16,3,bias=False,padding=1),
+        nn.ReLU(),
+        GroupNorm(4,16)
+        ) #32
+    self.convblock2 = nn.Sequential(
+        nn.Conv2d(16,32,3,bias=False,padding=1),
+        nn.ReLU(),
+        GroupNorm(4,32)
+        ) #32
+    self.convblock3 = nn.Sequential(
+        nn.Conv2d(32,16,1,bias=False,padding=1),
+        nn.ReLU(),
+        GroupNorm(4,16)
+        ) #32
+    self.pool1 = nn.MaxPool2d(2,2) #16
+
+    self.convblock4 = nn.Sequential(
+        nn.Conv2d(16,32,3,bias=False,padding=1),
+        nn.ReLU(),
+        GroupNorm(4,32)
+        ) #16
+    self.convblock5 = nn.Sequential(
+        nn.Conv2d(32,32,3,bias=False,padding=1),
+        nn.ReLU(),
+        GroupNorm(4,32)
+        ) #16
+    self.convblock6 = nn.Sequential(
+        nn.Conv2d(32,32,3,bias=False,padding=1),
+        nn.ReLU(),
+        GroupNorm(4,32)
+        ) #16
+    self.convblock7 = nn.Sequential(
+        nn.Conv2d(32,16,1,bias=False,padding=1),
+        nn.ReLU(),
+        GroupNorm(4,16)
+        ) #16
+
+    self.pool2= nn.MaxPool2d(2,2) #8
+
+    self.convblock8 = nn.Sequential(
+        nn.Conv2d(16,32,3,bias=False),
+        nn.ReLU(),
+        GroupNorm(4,32)
+        ) #6
+    self.convblock9 = nn.Sequential(
+        nn.Conv2d(32,32,3,bias=False),
+        nn.ReLU(),
+        GroupNorm(4,32)
+        ) #4
+
+    self.convblock10 = nn.Sequential(
+        nn.Conv2d(32,64,3,bias=False),
+        nn.ReLU(),
+        GroupNorm(4,64)
+        ) #2
+
+    self.gap = nn.Sequential(
+        nn.AdaptiveAvgPool2d(1)
+        ) #1
+
+    self.convblock11 = nn.Sequential(
+        nn.Conv2d(64,10,1,bias=False),
+        ) #1
+  def forward(self,x):
+    x= self.pool1(self.convblock3(self.convblock2(self.convblock1(x))))
+    x= self.pool2(self.convblock7(self.convblock6(self.convblock5(self.convblock4(x)))))
+    x= self.convblock10(self.convblock9(self.convblock8(x)))
+    x= self.gap(x)
+    x=self.convblock11(x)
+    x=x.view(-1,10)
+    return F.log_softmax(x)
+  
+
+
+from torch.nn.modules.normalization import GroupNorm
+class Model_layer_norm(nn.Module):
+  def __init__(self):
+    super(Model_layer_norm,self).__init__()
+    self.convblock1 = nn.Sequential(
+        nn.Conv2d(3,16,3,bias=False,padding=1),
+        nn.ReLU(),
+        GroupNorm(1,16)
+        ) #32
+    self.convblock2 = nn.Sequential(
+        nn.Conv2d(16,32,3,bias=False,padding=1),
+        nn.ReLU(),
+        GroupNorm(1,32)
+        ) #32
+    self.convblock3 = nn.Sequential(
+        nn.Conv2d(32,16,1,bias=False,padding=1),
+        nn.ReLU(),
+        GroupNorm(1,16)
+        ) #32
+    self.pool1 = nn.MaxPool2d(2,2) #16
+
+    self.convblock4 = nn.Sequential(
+        nn.Conv2d(16,32,3,bias=False,padding=1),
+        nn.ReLU(),
+        GroupNorm(1,32)
+        ) #16
+    self.convblock5 = nn.Sequential(
+        nn.Conv2d(32,32,3,bias=False,padding=1),
+        nn.ReLU(),
+        GroupNorm(1,32)
+        ) #16
+    self.convblock6 = nn.Sequential(
+        nn.Conv2d(32,32,3,bias=False,padding=1),
+        nn.ReLU(),
+        GroupNorm(1,32)
+        ) #16
+    self.convblock7 = nn.Sequential(
+        nn.Conv2d(32,16,1,bias=False,padding=1),
+        nn.ReLU(),
+        GroupNorm(1,16)
+        ) #16
+
+    self.pool2= nn.MaxPool2d(2,2) #8
+
+    self.convblock8 = nn.Sequential(
+        nn.Conv2d(16,32,3,bias=False),
+        nn.ReLU(),
+        GroupNorm(1,32)
+        ) #6
+    self.convblock9 = nn.Sequential(
+        nn.Conv2d(32,32,3,bias=False),
+        nn.ReLU(),
+        GroupNorm(1,32)
+        ) #4
+
+    self.convblock10 = nn.Sequential(
+        nn.Conv2d(32,64,3,bias=False),
+        nn.ReLU(),
+        GroupNorm(1,64)
+        ) #2
+
+    self.gap = nn.Sequential(
+        nn.AdaptiveAvgPool2d(1)
+        ) #1
+
+    self.convblock11 = nn.Sequential(
+        nn.Conv2d(64,10,1,bias=False),
+        ) #1
+  def forward(self,x):
+    x= self.pool1(self.convblock3(self.convblock2(self.convblock1(x))))
+    x= self.pool2(self.convblock7(self.convblock6(self.convblock5(self.convblock4(x)))))
+    x= self.convblock10(self.convblock9(self.convblock8(x)))
+    x= self.gap(x)
+    x=self.convblock11(x)
+    x=x.view(-1,10)
+    return F.log_softmax(x)
+
